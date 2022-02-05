09feed83dccaa80797a5ee335dad883fd2e6ad1c
bettersan <bettersan@naver.com>
2022-02-05 08:45:15 +0000

    merge

--- node/app.js
+++ node/app.js
@@ -8,14 +8,7 @@ app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 app.use(express.static('public'));
 
-<<<<<<< HEAD
-
 app.listen(PORT, () => {
     console.log('Server On : http://localhost:${PORT}/');
 })
-=======
-app.listen(PORT, ()=> {
-	console.log('Server On : ${PORT}');
-})
 
->>>>>>> 8807e8dfb74778121f0709a893d0ccfd05d73e9d
